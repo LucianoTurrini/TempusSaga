cb7533c3a69344f53a866c2247e4bd7c30d8a1ae
SidneyDuarte <sidney_silva13@outlook.com>
2015-05-14 11:39:29 -0300

    Cena inicial e classes de view

---
+++ Tempus Saga/Tempus Saga/SceneViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SceneViewController.swift
+//  Tempus Saga
+//
+//  Created by Sidney Silva on 5/14/15.
+//  Copyright (c) 2015 Luciano Moreira Turrini. All rights reserved.
+//
+
+import UIKit
+
+class SceneViewController: UIViewController {
+
+    @IBOutlet weak var imageCharacter: UIImageView!
+    @IBOutlet weak var imageCharacter2: UIImageView!
+    @IBOutlet weak var labelSpeak: UILabel!
+    @IBOutlet weak var imageSpeakBackground: UILabel!
+    @IBOutlet weak var imageBackground: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Tempus Saga/Tempus Saga/ViewController/StageViewController.swift
@@ -0,0 +1,35 @@
+//
+//  StageViewController.swift
+//  Tempus Saga
+//
+//  Created by Sidney Silva on 5/14/15.
+//  Copyright (c) 2015 Luciano Moreira Turrini. All rights reserved.
+//
+
+import UIKit
+
+class StageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
