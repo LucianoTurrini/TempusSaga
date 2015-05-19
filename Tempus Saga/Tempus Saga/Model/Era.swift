ab718623ef0f1afc2dfc740c3ab1c0f7075e72d2
LucianoTurrini <luciano_turrini@hotmail.com>
2015-05-19 08:41:14 -0300

    novos modelos

---
+++ Tempus Saga/Tempus Saga/Model/Era.swift
@@ -0,0 +1,18 @@
+//
+//  Era.swift
+//  Tempus Saga
+//
+//  Created by Luciano Moreira Turrini on 5/18/15.
+//  Copyright (c) 2015 Luciano Moreira Turrini. All rights reserved.
+//
+
+import UIKit
+
+class Era: NSObject {
+    
+    var nome: String = ""
+    var places: Array<String>!
+    var imageBackground: String = ""
+
+   
+}
