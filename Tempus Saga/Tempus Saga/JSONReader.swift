fa9dcc33d1690f1ca25fb796781bcb4f8e4c7aa7
Jorge H. P. Garcia <j.hrique@hotmail.com>
2015-05-21 18:05:02 -0300

    fix de imagem teste na história

--- Tempus Saga/Tempus Saga/JSONReader.swift
+++ Tempus Saga/Tempus Saga/JSONReader.swift
@@ -122,31 +122,31 @@ class JSONReader: NSObject {
     
     
     
-    class func getFalas (arquivo: String) -> Array<[String]> {  // Remover
-    
-        var textAndImages = Array<[String]>()
-        
-        ///// Teste ////////////
-        let fala1:[String] = ["1", "- Hey man! What's up?", "char1"]
-        let fala2:[String] = ["2", "- I'm ok.", ""]
-        let fala3:[String] = ["2", "- And you?", ""]
-        let fala4:[String] = ["1", "- Ok.", ""]
-        let fala5:[String] = ["1", "- ...", ""]
-        let fala6:[String] = ["2", "- ...", ""]
-        let fala7:[String] = ["1", "- Bye.", ""]
-        let fala8:[String] = ["2", "- Bye.", ""]
-        textAndImages.append(fala1)
-        textAndImages.append(fala2)
-        textAndImages.append(fala3)
-        textAndImages.append(fala4)
-        textAndImages.append(fala5)
-        textAndImages.append(fala6)
-        textAndImages.append(fala7)
-        textAndImages.append(fala8)
-        ///// Teste ////////////
-    
-        return textAndImages
-    }
+//    class func getFalas (arquivo: String) -> Array<[String]> {  // Remover
+//    
+//        var textAndImages = Array<[String]>()
+//        
+//        ///// Teste ////////////
+//        let fala1:[String] = ["1", "- Hey man! What's up?", "char1"]
+//        let fala2:[String] = ["2", "- I'm ok.", ""]
+//        let fala3:[String] = ["2", "- And you?", ""]
+//        let fala4:[String] = ["1", "- Ok.", ""]
+//        let fala5:[String] = ["1", "- ...", ""]
+//        let fala6:[String] = ["2", "- ...", ""]
+//        let fala7:[String] = ["1", "- Bye.", ""]
+//        let fala8:[String] = ["2", "- Bye.", ""]
+//        textAndImages.append(fala1)
+//        textAndImages.append(fala2)
+//        textAndImages.append(fala3)
+//        textAndImages.append(fala4)
+//        textAndImages.append(fala5)
+//        textAndImages.append(fala6)
+//        textAndImages.append(fala7)
+//        textAndImages.append(fala8)
+//        ///// Teste ////////////
+//    
+//        return textAndImages
+//    }
 
     
     
