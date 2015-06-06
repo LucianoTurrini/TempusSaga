79a1e00569abee00d6ff0599e0d04e8ae988ad35
Jorge H. P. Garcia <j.hrique@hotmail.com>
2015-06-06 01:10:46 -0300

    Botão voltar adicionado no oráculo e pequenos ajustes no storyboard

--- Tempus Saga/Tempus Saga/ViewController/OracleViewController.swift
+++ Tempus Saga/Tempus Saga/ViewController/OracleViewController.swift
@@ -46,6 +46,10 @@ class OracleViewController: UIViewController {
     
     // Mark: Methods
     
+    @IBAction func btVoltar(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
